9adb2ab47e9006389782bc9f135086fd11366c31
Sebastian Mueller <sebastian@sourceboat.com>
2017-07-09 12:02:23 +0200

    Add pug, add/modify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 const path = require('path')
 const elixir = require('laravel-elixir')
+require('laravel-elixir-pug')
+
+const clientSrcPath = './client'
+const clientDestPath = './public'
 
 elixir(mix => {
   // scripts
   mix.browserify(
-    './app/js/main.js',
-    './public/js'
+    clientSrcPath + '/assets/scripts/app.js',
+    clientDestPath + '/js'
   )
+
+  mix.pug({
+      blade: false,
+      pretty: true,
+      src: clientSrcPath + '/views/',
+      search: '**/*.pug',
+      pugExtension: '.pug',
+      dest: clientDestPath + '/views'
+  })
 })
